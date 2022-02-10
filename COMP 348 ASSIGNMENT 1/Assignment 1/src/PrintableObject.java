d37b588c6afad312b845a29682ad3d54381ad449
Gaiyd <84110466+Gaiyd@users.noreply.github.com>
2022-02-09 20:36:37 -0500

    java part

---
+++ COMP 348 ASSIGNMENT 1/Assignment 1/src/PrintableObject.java
@@ -0,0 +1,11 @@
+public abstract class PrintableObject implements NamedObject, Printable {
+    
+    public String toString(){
+        return this.getName();
+    }
+
+    public void print(){
+        System.out.println(toString());
+    }
+    
+}
